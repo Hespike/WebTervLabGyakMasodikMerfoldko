ff6e4fd1140aef1ebe5df428791651f52cb20207
Sárdi András <sardi.andras@gmail.com>
2021-04-21 16:11:18 +0200

    Update tartalom.php

--- tartalom.php
+++ tartalom.php
@@ -1,20 +1,4 @@
-//class=lablec, szoveg_lab és lab hibát ír
 
-<?php
-function footer(){
-    echo "            
-     <footer  class="lablec">
-                <p class="szoveg_lab">Megbízható, precíz szakembert keres? Keressen minket!</p>
-                <br>
-                <hr>
-                <br>
-                <p class="lab">Copyright &copy; Deli Dorottya és Sárdi András Mihály, 2021</p>
-            </footer>
-            ";
-}
-?>
-
-/* Így működik
 <?php
 function footer(){
     echo "            
@@ -28,4 +12,4 @@ function footer(){
             ";
 }
 ?>
-*/
+
