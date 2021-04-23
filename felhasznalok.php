02363792214c7e168ac1ba7ef6ce37dff19b2abb
Sárdi András <sardi.andras@gmail.com>
2021-04-23 13:14:31 +0200

    Update felhasznalok.php

--- felhasznalok.php
+++ felhasznalok.php
@@ -1,12 +1,12 @@
 <?php
-include_once "Users.php";
+include_once "User.php";
 session_start();
 $file = fopen("users.txt", "a+");
 $_SESSION["regisztraltFelhasznalok"] = [];
-while($line = fgets($file) !== false){
+while(($line = fgets($file)) !== false){
     $sor = unserialize($line);
-    $newFelhasznalo = new Users($sor["nev"],$sor["fnev"], $sor["jelszo"],$sor["jelszoujra"], $sor["telszam"], $sor["email"], $sor["kep"]);
-    array_push($_SESSION["regisztraltFelhasznalok"], $newFelhasznalo);
+    $newUser= new User($sor["nev"],$sor["fnev"], $sor["jelszo"],$sor["jelszoujra"], $sor["telszam"], $sor["email"], $sor["kep"]);
+    array_push($_SESSION["regisztraltFelhasznalok"], $newUser);
 }
 fclose($file);
 ?>
